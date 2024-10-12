f23335cbadcae6de8bfce077b22a4d667f470391
Ahmed Nedjm Eddine <89082915+Mnedjm28@users.noreply.github.com>
2024-10-12 23:17:01 +0100

    Change name of SharedtoolController to SharedToolController and pass SharedToolList to index view

--- ShareTool/ShareTool/Controllers/SharedToolController.cs
+++ ShareTool/ShareTool/Controllers/SharedToolController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ShareTool.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +12,13 @@ namespace ShareTool.Controllers
         // GET: Sharedtool
         public ActionResult Index()
         {
-            return View();
+            return View(new List<SharedTool>()
+            {
+                new SharedTool("غسالة","غسالة مستعملة بحالة مية بالمية",1),
+                new SharedTool("دراجة","دراجة هوائية كبيرة الحجم يمكن استخدامه ",4),
+                new SharedTool("تلفاز","تلفاز مكسور النصف العلوي من شاشته",2),
+                new SharedTool("كمبيوتر","كمبيوتر بمعالج i5 بحالة جيدة",2),
+            });
         }
     }
 }
