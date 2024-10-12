8c499d46e4a8ef85c20d743fe1184b91a161c48a
Ahmed Nedjm Eddine <89082915+Mnedjm28@users.noreply.github.com>
2024-10-13 00:46:58 +0100

    Add create action in get & post with it view

--- ShareTool/ShareTool/Controllers/SharedToolController.cs
+++ ShareTool/ShareTool/Controllers/SharedToolController.cs
@@ -9,16 +9,29 @@ namespace ShareTool.Controllers
 {
     public class SharedToolController : Controller
     {
-        // GET: Sharedtool
-        public ActionResult Index()
-        {
-            return View(new List<SharedTool>()
+        public List<SharedTool> sharedTools = new List<SharedTool>()
             {
                 new SharedTool("غسالة","غسالة مستعملة بحالة مية بالمية",1),
                 new SharedTool("دراجة","دراجة هوائية كبيرة الحجم يمكن استخدامه ",4),
                 new SharedTool("تلفاز","تلفاز مكسور النصف العلوي من شاشته",2),
                 new SharedTool("كمبيوتر","كمبيوتر بمعالج i5 بحالة جيدة",0),
-            });
+            };
+        // GET: Sharedtool
+        public ActionResult Index()
+        {
+            return View(sharedTools);
+        }
+
+        public ActionResult Create() {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create([Bind (Include ="Name, Quantity", Exclude ="Description")] FormCollection values) {
+            var name = values[nameof(SharedTool.Name)];
+            var description = values[nameof(SharedTool.Description)];
+            var quantity = values[nameof(SharedTool.Quantity)];
+            return RedirectToAction("Index");
         }
 
         public ActionResult RequestTool() {
