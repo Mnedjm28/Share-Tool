69a851899447ccdde3705b332544f551c9ee49e3
Ahmed Nedjm Eddine <89082915+Mnedjm28@users.noreply.github.com>
2024-10-12 23:01:50 +0100

    Add SharedTool model

---
+++ ShareTool/ShareTool/Models/SharedTool.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShareTool.Models
+{
+    public class SharedTool
+    {
+        public SharedTool(string name, string description, int quantity)
+        {
+            Name = name;
+            Description = description;
+            Quantity = quantity;
+        }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
