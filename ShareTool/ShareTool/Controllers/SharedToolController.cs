58221d9f0015c363d2a41eaecccaabd6d18ec066
Ahmed Nedjm Eddine <89082915+Mnedjm28@users.noreply.github.com>
2024-10-12 23:02:31 +0100

    Add SharedTool controller

---
+++ ShareTool/ShareTool/Controllers/SharedToolController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShareTool.Controllers
+{
+    public class SharedToolController : Controller
+    {
+        // GET: Sharedtool
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
