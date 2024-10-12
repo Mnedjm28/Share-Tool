a2c06877a80b52800c784560a7fece2297c3263a
Ahmed Nedjm Eddine <89082915+Mnedjm28@users.noreply.github.com>
2024-10-13 00:45:09 +0100

    Update RouteConfig : change default route

--- ShareTool/ShareTool/App_Start/RouteConfig.cs
+++ ShareTool/ShareTool/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ShareTool
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "SharedTool", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
