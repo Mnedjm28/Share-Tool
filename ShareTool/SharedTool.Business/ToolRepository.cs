207567c3c2be129547f256be3534edfff2475bb9
Ahmed Nedjm Eddine <89082915+Mnedjm28@users.noreply.github.com>
2024-10-14 11:59:44 +0100

    Fill DAL by migrate db model by Database first

---
+++ ShareTool/SharedTool.Business/ToolRepository.cs
@@ -0,0 +1,48 @@
+﻿using SharedTool.DAL;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedTool.Business
+{
+    class ToolRepository
+    {
+        public async Task<List<Tool>> GetAllTools()
+        {
+            var context = new SharedToolDb();
+            return await context.Tools.ToListAsync();
+        }
+
+        public async Task<Tool> GetToolById(int id)
+        {
+            var context = new SharedToolDb();
+            return await context.Tools.FindAsync(id);
+        }
+
+        public async Task Add(Tool tool)
+        {
+            var context = new SharedToolDb();
+            context.Tools.Add(tool);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task Delete(int id)
+        {
+            var context = new SharedToolDb();
+            var tool = await context.Tools.FindAsync(id);
+            context.Tools.Remove(tool);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task Update(Tool tool)
+        {
+            var context = new SharedToolDb();
+            var oldTool = await context.Tools.FindAsync(tool.Id);
+            context.Entry(oldTool).CurrentValues.SetValues(tool);
+            await context.SaveChangesAsync();
+        }
+    }
+}
---
+++ ShareTool/SharedTool.Business/UserRepository.cs
@@ -0,0 +1,48 @@
+﻿using SharedTool.DAL;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedTool.Business
+{
+    class UserRepository
+    {
+        public async Task<List<User>> GetAllTools()
+        {
+            var context = new SharedToolDb();
+            return await context.Users.ToListAsync();
+        }
+
+        public async Task<User> GetUserById(int id)
+        {
+            var context = new SharedToolDb();
+            return await context.Users.FindAsync(id);
+        }
+
+        public async Task Add(User user)
+        {
+            var context = new SharedToolDb();
+            context.Users.Add(user);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task Delete(int id)
+        {
+            var context = new SharedToolDb();
+            var user = await context.Users.FindAsync(id);
+            context.Users.Remove(user);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task Update(User user)
+        {
+            var context = new SharedToolDb();
+            var oldUser = await context.Users.FindAsync(user.Id);
+            context.Entry(oldUser).CurrentValues.SetValues(user);
+            await context.SaveChangesAsync();
+        }
+    }
+}
