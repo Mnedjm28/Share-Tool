9c00fd5658890d047754b6682ffe8bee06caf2d3
Ahmed Nedjm Eddine <89082915+Mnedjm28@users.noreply.github.com>
2024-10-14 13:47:05 +0100

    Integrate authentication by asp .net Identity

---
+++ ShareTool/ShareTool/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System.Runtime.InteropServices;
+
+[assembly: OwinStartupAttribute(typeof(ShareTool.Startup))]
+namespace ShareTool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
